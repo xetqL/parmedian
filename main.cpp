cc83d57c05143b3bab3dd96e9472a20ffc0a1378
Anthony Boulmier <anthony.boulmier@unige.ch>
2020-09-09 17:52:28 +0200

    add partial medians

--- main.cpp
+++ main.cpp
@@ -23,32 +23,42 @@ int main(int argc, char** argv){
     int ws,rk;
     MPI_Comm_size(MPI_COMM_WORLD, &ws);
     MPI_Comm_rank(MPI_COMM_WORLD, &rk);
-	const int S = 99999;
+	const int S = 100000;
 	vector<double> v(S), r(S * ws);
     srand(rk + time(NULL));
 	int MAX_TRIAL = 10;
 	for(int trial = 0; trial < MAX_TRIAL; ++trial){
 	    if(!rk) cout << "trial - " << trial << endl;
-        generate(v.begin(), v.end(), []() { return (double) (rand() / (double) RAND_MAX); });
-        MPI_Gather(v.data(), v.size(), par::get_mpi_type<decltype(v)::value_type>(), r.data(), v.size(), par::get_mpi_type<decltype(v)::value_type>(), 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+        generate(v.begin(), v.end(), []() { return (double) rand() / (double) RAND_MAX ; });
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto part1  = steady_clock::now();
+        auto parmed = par::median(begin(v), end(v));
+        auto part2  = steady_clock::now();
         MPI_Barrier(MPI_COMM_WORLD);
 
-        auto part1 = steady_clock::now();
-        auto parmed= par::median(begin(v), end(v));
-        auto part2 = steady_clock::now();
+        auto seqt1 = steady_clock::now();
+        MPI_Gather(v.data(), v.size(), par::get_mpi_type<decltype(v)::value_type>(), r.data(), v.size(), par::get_mpi_type<decltype(v)::value_type>(), 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto seqt2 = steady_clock::now();
+        auto tupload = duration_cast<milliseconds>(seqt2-seqt1).count();
+        std::cout << std::setw(10) << "t upload : " << format(tupload) << std::endl;
 
         if(!rk) {
             std::cout << "par median: "  << format(parmed) << format(duration_cast<milliseconds>(part2-part1).count()) << std::endl;
             auto seqt1 = steady_clock::now();
-            auto med   = median(r);
+            //std::nth_element(r.begin(),  (r.begin() + (r.size() / 2)), r.end());
+            float med   = *(r.begin() + r.size() / 2);
             auto seqt2 = steady_clock::now();
-
-            std::cout << std::setw(10) << "qs  median: " << format(med) << format(duration_cast<milliseconds>(seqt2-seqt1).count()) << std::endl;
+            std::cout << std::setw(10) << "qs  median: " << format(med) << format((duration_cast<milliseconds>(seqt2-seqt1).count()  + tupload)) << std::endl;
             seqt1 = steady_clock::now();
-            med   = nlogn_median(r);
+            //med   = nlogn_median(r);
+
             seqt2 = steady_clock::now();
             std::cout << std::setw(10) << "sortmedian: " << format(med) << format(duration_cast<milliseconds>(seqt2-seqt1).count()) << std::endl;
-            assert(med == parmed);
+            //assert(med == parmed);
+
         }
     }
 	MPI_Finalize();
--- median.hpp
+++ median.hpp
@@ -7,7 +7,7 @@
 #include <vector>
 #include <mpi.h>
 #include <random>
-
+#include <iterator>
 template<class T> void split_vec(const std::vector<T>& vec, T pivot, std::vector<T>& le, std::vector<T>& gr, std::vector<T>& pi){
     le.clear();
     gr.clear();
@@ -29,7 +29,7 @@ template<class T> double nlogn_median(std::vector<T> v){
     if(v.size() % 2)
         return v.at((v.size() / 2.0));
     else
-        return 0.5 * (v.at(v.size() / 2 - 1) + v.at(v.size() / 2));
+        return  (double) (v.at(v.size() / 2 - 1) + v.at(v.size() / 2)) / 2.0;
 }
 template<class T> double median(std::vector<T> x, size_t look_for) {
     std::random_device rd;
@@ -54,10 +54,38 @@ template<class T> double median(std::vector<T> x){
         return 0.5 * (median(x, x.size() / 2 - 1) + median(x, x.size() / 2));
     }
 }
+
+template<class InputIt>
+void partial_sort(InputIt begin, InputIt end, int stride) {
+    auto itp = begin, itn = itp;
+    while(itn != end) {
+        while(itn != end && std::distance(itp, itn) < stride){
+            itn++;
+        }
+        std::sort(itp, itn);
+        itp = itn;
+    }
+}
+template<class InputIt>
+std::vector<typename std::iterator_traits<InputIt>::value_type> partial_medians(InputIt begin, InputIt end, int stride)  {
+    std::vector<typename std::iterator_traits<InputIt>::value_type> medians; medians.reserve(std::distance(begin, end) / stride);
+    auto itp = begin, itn = itp;
+    while(itn != end) {
+        int _stride = stride;
+        while(itn != end && _stride > 0){
+            itn++;
+            _stride--;
+        }
+        std::sort(itp, itn);
+        medians.push_back(*(itp + (stride-_stride)/2));
+        itp = itn;
+    }
+    return medians;
+}
 namespace par {
     int ws,rk;
     template<class T>
-    MPI_Datatype get_mpi_type(){
+    constexpr MPI_Datatype get_mpi_type(){
         if constexpr (std::is_same<T, float>::value)                    return MPI_FLOAT;
         if constexpr (std::is_same<T, double>::value)                   return MPI_DOUBLE;
         if constexpr (std::is_same<T, int>::value)                      return MPI_INT;
@@ -72,12 +100,12 @@ namespace par {
         if constexpr (std::is_same<T, short>::value)                    return MPI_SHORT;
         if constexpr (std::is_same<T, short int>::value)                return MPI_SHORT_INT;
         if constexpr (std::is_same<T, char>::value)                     return MPI_CHAR;
-        return MPI_DATATYPE_NULL;
+        else return MPI_DATATYPE_NULL;
     }
+
     namespace {
     template<class T>
     double median(std::vector<T> x, size_t look_for) {
-        std::random_device rd;
         MPI_Comm_size(MPI_COMM_WORLD, &ws);
         MPI_Comm_rank(MPI_COMM_WORLD, &rk);
         /**
@@ -87,42 +115,55 @@ namespace par {
          * 3. All: keeps le if SUM(le_p) > SUM(gr_p) for all p in procs
          * 4. Go back to 1.
          */
-        std::vector<T> le, gr, pi;
-        std::array<size_t, 3> split_sizes{};
+        size_t cnt;
+        std::array<size_t, 2> split_sizes {};
+        std::array<T, 2> pivot_msg{};
+        std::vector<T> unfiltered_medians(2*ws);
+        std::vector<T> all_medians(ws);
+        constexpr T zero = (T) 0, one = (T) 1;
+        auto itp = x.begin(), itn = x.end();
+        T pivot;
         do {
-            size_t size = x.size(), total_size, lb, ub, ipivot;
-            T pivot;
-            MPI_Reduce(&size, &total_size, 1, get_mpi_type<size_t>(), MPI_SUM, 0, MPI_COMM_WORLD);
-            MPI_Scan(&size, &ub, 1, get_mpi_type<size_t>(), MPI_SUM, MPI_COMM_WORLD);
-            lb = ub - size;
-            if (!rk) {
-                ipivot = (rd() % total_size);
-            }
-            MPI_Bcast(&ipivot, 1, get_mpi_type<size_t>(), 0, MPI_COMM_WORLD);
-            if(lb <= ipivot && ipivot < ub) {
-                pivot = x.at(ipivot - lb);
-                for(auto pe = 0; pe < ws; ++pe)
-                    if(pe != rk) MPI_Send(&pivot, 1, get_mpi_type<T>(), pe, 999, MPI_COMM_WORLD);
+            //
+            T median_of_medians;
+            auto medians = partial_medians(itp, itn, 5);
+            std::nth_element(medians.begin(), medians.end(), medians.begin() + medians.size() / 2);
+            if(!medians.empty()) {
+                pivot_msg.at(0) = one;
+                pivot_msg.at(1) = *(medians.begin() + medians.size() / 2);
             } else {
-                MPI_Recv(&pivot, 1, get_mpi_type<T>(), MPI_ANY_SOURCE, 999, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+                pivot_msg.at(0) = zero;
             }
+            MPI_Gather(pivot_msg.data(), 2, get_mpi_type<T>(), unfiltered_medians.data(), 2, get_mpi_type<T>(), 0, MPI_COMM_WORLD);
 
-            auto li = std::partition(x.begin(), x.end(), [&pivot](auto v){return v < pivot;});
-            auto pi = std::partition(li, x.end(), [&pivot](auto v){return v == pivot;});
+            cnt = 0;
+            for(size_t i = 0; i < ws; ++i) {
+                all_medians.at(cnt) = unfiltered_medians.at(2*i+1);
+                cnt += unfiltered_medians.at(2*i);
+            }
+            if(!rk) {
+                std::nth_element(all_medians.begin(), all_medians.begin()+cnt, all_medians.begin() + (cnt / 2));
+                pivot = *(all_medians.begin() + (cnt / 2));
+            }
+            MPI_Bcast(&pivot, 1, get_mpi_type<T>(), 0, MPI_COMM_WORLD);
 
-            split_sizes = {std::distance(x.begin(), li), std::distance(li, pi)};
+            auto li = std::partition(itp, itn, [pivot](const auto& v){return v < pivot;});
+            auto pi = std::partition(li,  itn, [pivot](const auto& v){return v == pivot;});
+
+            split_sizes = {std::distance(itp, li), std::distance(li, pi)};
 
             MPI_Allreduce(MPI_IN_PLACE, &split_sizes, 2, get_mpi_type<size_t>(), MPI_SUM, MPI_COMM_WORLD);
 
             if(look_for < split_sizes[0]) {
-                x = std::vector<T>(x.begin(), li);
-            } else if (look_for < split_sizes[0] + split_sizes[1]) {
-                return pivot;
-            } else {
-                x = std::vector<T>(pi, x.end());
+                itn = li;
+            } else if(look_for >= split_sizes[0] + split_sizes[1]){
+                itp = pi;
                 look_for = look_for - split_sizes[0] - split_sizes[1];
             }
-        } while(true);
+
+        } while (!(look_for >= split_sizes[0] && look_for < split_sizes[0] + split_sizes[1]));
+
+        return pivot;
     }
     }
     template<class T>
